bd0b08796085a71f5778861274160709d5486907
Karol Wołonciej <kwolonciej@gmail.com>
2022-06-02 23:00:31 +0200

    dodano struktury do openMP sort

--- makefile
+++ makefile
@@ -4,7 +4,7 @@ MPICC?=g++
 all: ${EXECS}
 
 zadanie2: zadanie2.cpp
-	${MPICC} -fopenmp -o zadanie2 zadanie2.cpp
+	${MPICC} -fopenmp zadanie2.cpp -o zadanie2 
 
 clean:
 	rm -f ${EXECS}
--- zadanie2.cpp
+++ zadanie2.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <stdlib.h>
 #include <time.h>
+#include "sortsOpenMP.cpp"
 
 #include <omp.h>
 // #include <mpi.h>
@@ -14,40 +15,9 @@ using namespace std;
 
 
 
-int get_block_start(int blockId, int blocksNumber, int dataSize) {
-	return (dataSize / blocksNumber) * blockId;
-}
 
-void local_sort_openMP(int64* A, int size) {
 
-	// posortowac wszystkie czesci lokalnie
-	int blocksNumber = 0;
-	#pragma omp parallel
-	{
-		blocksNumber = omp_get_num_threads();
-		int lastElemensSize = size % blocksNumber;
-		int blockId = omp_get_thread_num();
-		int blockStart = get_block_start(blockId, blocksNumber, size);
-		int blockEnd = get_block_start(blockId+1, blocksNumber, size) + (blockId == blocksNumber-1 ? lastElemensSize : 0);
-		std::sort(A + blockStart, A + blockEnd, less<int64>());  //tutaj dodac min zeby nie wywalo 
-	}
-
-	int merges = blocksNumber / 2;
-	for (int mergeStep = 1; mergeStep < blocksNumber; mergeStep *= 2 )
-	{
-		int mergesInStep = (blocksNumber / (2 * mergeStep));
-
-		#pragma omp parallel for
-		for (int i = 0; i < mergesInStep; i++) {
-			int halfMergeLen = (size / blocksNumber) * mergeStep;
-			int mergeStart = i * 2 * halfMergeLen;
-			int mergeMid = mergeStart + halfMergeLen;
-			int mergeEnd = i == mergesInStep-1 ? size : mergeStart + 2 * halfMergeLen;
-			mergeEnd = min(mergeEnd, size);
-			inplace_merge(A + mergeStart, A + mergeMid, A + mergeEnd, less<int64>());
-		}
-	}
-}
+// void sample_sort_MPI()
 
 
 
@@ -71,7 +41,7 @@ int main(int argc, char** argv) {
 	// cout<<endl<<endl;
 
 
-	local_sort_openMP(A, size);
+	// local_sort_openMP(A, size);
 
 	// std::sort(A_test, A_test + size, less<int64>());
 	
