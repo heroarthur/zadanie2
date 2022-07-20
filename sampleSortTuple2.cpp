63cc0e86ea7d6c21ab0dcf16dde80378f1fd7be1
Karol Wołonciej <kwolonciej@gmail.com>
2022-07-20 19:09:38 +0200

    a

--- sampleSortTuple2.cpp
+++ sampleSortTuple2.cpp
@@ -196,7 +196,7 @@ void mergeSortedParts(vector<Tuple2>* A,
 	{
 		int mergesInStep = (blocksNumberWithPadding / (2 * mergeStep));
 
-        #pragma omp parallel for
+        #pragma omp parallel for num_threads(THREADS_NUM)
 		for (int i = 0; i < mergesInStep; i++) {
             int64 indexMergeStart = 2 * mergeStep * i;
             int64 indexMergeMid = indexMergeStart + mergeStep;
